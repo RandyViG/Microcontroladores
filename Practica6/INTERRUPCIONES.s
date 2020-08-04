0dd55b845c280fed90b690c18f325d7e7d523f27
RandyVig <rvillanuevag1603@alumno.ipn.mx>
2020-08-04 18:51:46 -0500

    Corrigiendo variables globales

--- Practica6/INTERRUPCIONES.s
+++ Practica6/INTERRUPCIONES.s
@@ -7,14 +7,15 @@
 ;*  @param: W0 tiene el dato recibido
 ;*  @return: ninguno ( void )
 __U1RXInterrupt:
-    PUSH    W0
-    CLR	    W0
-    MOV	    U1RXREG,	    W0
-    MOV.B   WREG,	    _dato
-    MOV	    #1,		    _drcv
-    BCLR    IFSO,	    #U1RXIF
-    POP	    W0
+    PUSH	W0
+    CLR		W0
+    MOV		U1RXREG,	W0
+    MOV.B	WREG,		_dato
+    BSET	_drcv,	    	#0
+    BCLR	IFS0,	    	#U1RXIF
+    POP		W0
     
     RETFIE
     
-    
+  
+  
